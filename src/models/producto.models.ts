169e1f2b218f845aba19252ab6a33d5211fd70f4
Belen <belenburgos.dev@gmail.com>
2025-12-01 00:54:54 -0300

    arreglos en models

--- src/models/producto.models.ts
+++ src/models/producto.models.ts
@@ -1,20 +1,26 @@
-import { Categoria } from "./categoria.models";
 export class Producto {
-    idProducto: number;
-    nombre: string;
-    precio?: number;
-    descripcion: string;
-    idCategoria: Categoria;
-    stock: number;
+  idProducto: number;
+  nombre: string;
+  precio?: number;
+  descripcion: string;
+  idCategoria: number;
+  stock: number;
 
-    constructor(idProducto: number, nombre: string, descripcion: string, idCategoria: Categoria, stock: number, precio?: number) {
-        this.idProducto = idProducto;
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-        this.idCategoria = idCategoria;
-        this.stock = stock;
-        if (precio !== undefined) {
-            this.precio = precio;
-        }
+  constructor(
+    idProducto: number,
+    nombre: string,
+    descripcion: string,
+    idCategoria: number,
+    stock: number,
+    precio?: number
+  ) {
+    this.idProducto = idProducto;
+    this.nombre = nombre;
+    this.descripcion = descripcion;
+    this.idCategoria = idCategoria;
+    this.stock = stock;
+    if (precio !== undefined) {
+      this.precio = precio;
     }
+  }
 }
